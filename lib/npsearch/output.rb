8189625c6d11de2491451d056ed020ca37669f0b
IsmailM <ismail.moghul@gmail.com>
2015-09-14 08:47:30 +0100

    make rubocop happy

--- lib/npsearch/output.rb
+++ lib/npsearch/output.rb
@@ -5,17 +5,15 @@ module NpSearch
   class Output
     class << self
       def to_html(input_file)
-        output_file = "#{input_file}.out.html"
         templates_path = File.expand_path(File.join(__FILE__, '../../../',
                                                     'templates/contents.slim'))
         contents_temp = File.read(templates_path)
         html_content = Slim::Template.new { contents_temp }.render(NpSearch)
-        File.open(output_file, 'w') { |f| f.puts html_content }
+        File.open("#{input_file}.out.html", 'w') { |f| f.puts html_content }
       end
 
       def to_fasta(input_file, sorted_sequences, input_type)
-        output_file = "#{input_file}.out.fa"
-        File.open(output_file, 'w') do |f|
+        File.open("#{input_file}.out.fa", 'w') do |f|
           sorted_sequences.each do |s|
             if input_type == :protein
               f.puts ">#{s.id}\n#{s.signalp}#{s.seq}"
