5e5e351214b2b64328f356839ca299b462a04b03
IsmailM <ismail.moghul@gmail.com>
2015-09-17 15:12:36 +0100

    remove the usage of tempfiles in running signalp

--- lib/npsearch/signalp.rb
+++ lib/npsearch/signalp.rb
@@ -1,5 +1,4 @@
 require 'forwardable'
-require 'tempfile'
 
 module NpSearch
   # A class to hold sequence data
@@ -11,14 +10,9 @@ module NpSearch
       def analyse_sequence(seq)
         sp_headers = %w(name cmax cmax_pos ymax ymax_pos smax smax_pos smean d
                         sp dmaxcut networks)
-        f = Tempfile.new('signalp')
-        f.write(">seq\n#{seq}")
-        f.close
-        s = `#{opt[:signalp_path]} -t euk -f short -U 0.3 -u 0.3 '#{f.path}' | \
-             sed -n '3 p'`
+        s = `echo ">seq\n#{seq}\n" | #{opt[:signalp_path]} -t euk -f short \
+             -U 0.3 -u 0.3 | sed -n '3 p'`
         Hash[sp_headers.map(&:to_sym).zip(s.split)]
-      ensure
-        f.unlink
       end
     end
   end
