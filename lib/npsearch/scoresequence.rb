701874899ea9dc67b94e059048c921820a16ae18
IsmailM <ismail.moghul@gmail.com>
2015-09-18 00:23:22 +0100

    make @seqeunce a local variable

--- lib/npsearch/scoresequence.rb
+++ lib/npsearch/scoresequence.rb
@@ -13,92 +13,92 @@ module NpSearch
                "(#{MONO_NP_CLV_6})"
 
       def run(sequence, temp_dir)
-        @sequence = sequence
-        split_into_neuropeptides
-        count_np_cleavage_sites
-        count_c_terminal_glycines
-        np_similarity(temp_dir)
-        acidic_spacers
+        split_into_neuropeptides(sequence)
+        count_np_cleavage_sites(sequence)
+        count_c_terminal_glycines(sequence)
+        np_similarity(sequence, temp_dir)
+        acidic_spacers(sequence)
       end
 
       private
 
-      def split_into_neuropeptides
+      def split_into_neuropeptides(sequence)
         potential_nps = []
-        results = @sequence.seq.scan(/(?<=^|#{NP_CLV})(\w+?)(?=#{NP_CLV}|$)/i)
+        results = sequence.seq.scan(/(?<=^|#{NP_CLV})(\w+?)(?=#{NP_CLV}|$)/i)
         headers = %w(di_clv_st mono_2_clv_st mono_4_clv_st mono_6_clv_st np
                      di_clv_end mono_2_clv_end mono_4_clv_end mono_6_clv_end)
         results.each { |e| potential_nps << Hash[headers.map(&:to_sym).zip(e)] }
-        @sequence.potential_cleaved_nps = potential_nps
+        sequence.potential_cleaved_nps = potential_nps
+        sequence
       end
 
-      def count_np_cleavage_sites
-        return if @sequence.potential_cleaved_nps.length == 0
-        @sequence.potential_cleaved_nps.each do |e|
-          count_dibasic_np_clv(e[:di_clv_end])
-          count_mono_basic_np_clv(e[:mono_2_clv_end], e[:mono_4_clv_end],
-                                  e[:mono_6_clv_end])
+      def count_np_cleavage_sites(sequence)
+        return if sequence.potential_cleaved_nps.length == 0
+        sequence.potential_cleaved_nps.each do |e|
+          count_dibasic_np_clv(sequence, e[:di_clv_end])
+          count_mono_basic_np_clv(sequence, e[:mono_2_clv_end],
+                                  e[:mono_4_clv_end], e[:mono_6_clv_end])
         end
       end
 
-      def count_dibasic_np_clv(dibasic_clv)
+      def count_dibasic_np_clv(sequence, dibasic_clv)
         case dibasic_clv
         when 'KR'
-          @sequence.score += 0.09
+          sequence.score += 0.09
         when 'RR', 'KK'
-          @sequence.score += 0.05
+          sequence.score += 0.05
         end
       end
 
-      def count_mono_basic_np_clv(mono_2_clv, mono_4_clv, mono_6_clv)
+      def count_mono_basic_np_clv(sequence, mono_2_clv, mono_4_clv, mono_6_clv)
         return if mono_2_clv.nil? && mono_4_clv.nil? && mono_6_clv.nil?
-        @sequence.score += 0.02
+        sequence.score += 0.02
       end
 
       # Counts the number of C-terminal glycines
-      def count_c_terminal_glycines
-        return if @sequence.potential_cleaved_nps.length == 0
-        @sequence.potential_cleaved_nps.each do |e|
+      def count_c_terminal_glycines(sequence)
+        return if sequence.potential_cleaved_nps.length == 0
+        sequence.potential_cleaved_nps.each do |e|
           if e[:np] =~ /G$/ && e[:di_clv_end] == 'KR'
-            @sequence.score += 0.25
+            sequence.score += 0.25
           elsif e[:np] =~ /G$|GK$|GR$/
-            @sequence.score += 0.10
+            sequence.score += 0.10
           end
         end
       end
 
-      def acidic_spacers
-        @sequence.potential_cleaved_nps.each do |e|
+      def acidic_spacers(sequence)
+        sequence.potential_cleaved_nps.each do |e|
           acidic_residue = e[:np].count('DE')
           percentage_acidic = acidic_residue / e[:np].length
-          @sequence.score += 0.10 if percentage_acidic > 0.5
+          sequence.score += 0.10 if percentage_acidic > 0.5
         end
       end
 
-      def np_similarity(temp_dir)
-        results = run_uclust(temp_dir)
+      def np_similarity(sequence, temp_dir)
+        results = run_uclust(sequence, temp_dir)
         results.gsub!(/^[^C].*\n/, '')
         results.each_line do |c|
           cluster = c.split(/\t/)
           no_of_seq_in_cluster = cluster[3].to_i
           if no_of_seq_in_cluster > 1
-            @sequence.score += (0.15 * no_of_seq_in_cluster)
+            sequence.score += (0.15 * no_of_seq_in_cluster)
           end
         end
       end
 
-      def run_uclust(temp_dir)
+      def run_uclust(sequence, temp_dir)
         f = Tempfile.new('uclust', temp_dir)
         fo = Tempfile.new('uclust_out', temp_dir)
-        return unless write_sequence_content_to_tempfile(f)
+        return unless write_sequence_content_to_tempfile(sequence, f)
         `usearch -cluster_fast #{f.path} -id 0.5 -uc #{fo.path} >/dev/null 2>&1`
         IO.read(fo.path)
       end
 
-      def write_sequence_content_to_tempfile(tempfile)
-        return false if @sequence.potential_cleaved_nps.length == 0
+      def write_sequence_content_to_tempfile(sequence, tempfile)
+        return false if sequence.potential_cleaved_nps.length == 0
         content = ''
-        @sequence.potential_cleaved_nps.each_with_index do |e, i|
+        sequence.potential_cleaved_nps.each_with_index do |e, i|
           content += ">seq#{i}\n#{e[:np]}\n"
         end
         tempfile.write(content)
