0f9c1480c644044d6014d31d89790d5f8cf262fc
IsmailM <ismail.moghul@gmail.com>
2015-10-13 21:06:03 +0100

    gsub instead of piping to sed
    to decrease HOST CPU load

--- lib/npsearch/signalp.rb
+++ lib/npsearch/signalp.rb
@@ -12,8 +12,8 @@ module NpSearch
         sp_headers = %w(name cmax cmax_pos ymax ymax_pos smax smax_pos smean d
                         sp dmaxcut networks)
         s = `echo ">seq\n#{seq}\n" | #{opt[:signalp_path]} -t euk -f short \
-             -U 0.3 -u 0.3 | sed -n '3 p'`
-        Hash[sp_headers.map(&:to_sym).zip(s.split)]
+             -U 0.3 -u 0.3`
+        Hash[sp_headers.map(&:to_sym).zip(s.gsub(/^#.*\n/, '').split)]
       end
     end
   end
