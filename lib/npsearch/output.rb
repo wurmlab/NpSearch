94b4aad68ca85395f871daa32c0897888abe7979
IsmailM <ismail.moghul@gmail.com>
2015-11-16 17:11:20 +0000

    change output fasta file to '.npsearch.fa'

--- lib/npsearch/output.rb
+++ lib/npsearch/output.rb
@@ -9,12 +9,12 @@ module NpSearch
         templates_path = File.expand_path(File.join(__FILE__, '../../../',
                                                     'templates/contents.slim'))
         contents_temp = File.read(templates_path)
-        html_content = Slim::Template.new { contents_temp }.render(NpSearch)
-        File.open("#{input_file}.npsearch.html", 'w') { |f| f.puts html_content }
+        h_content = Slim::Template.new { contents_temp }.render(NpSearch)
+        File.open("#{input_file}.npsearch.html", 'w') { |f| f.puts h_content }
       end
 
       def to_fasta(input_file, sorted_sequences, input_type)
-        File.open("#{input_file}.out.fa", 'w') do |f|
+        File.open("#{input_file}.npsearch.fa", 'w') do |f|
           sorted_sequences.each do |s|
             if input_type == :protein
               f.puts ">#{s.defline}\n#{s.signalp}#{s.seq}"
